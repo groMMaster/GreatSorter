487cb2cdc25aa240843c4edab7f4ca3bffa61976
QmRite <lopesmigel9@gmail.com>
2022-12-10 19:39:10 +0500

    Init tests

---
+++ GreatSorter.Tests/Algorithms/BubbleSortTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatSorter.Tests.Algorithms
+{
+    internal class BubbleSortTests : AbstractAlgorithmsTests
+    {
+        [Test]
+        public void SortDefaultUnsortedArray()
+        {
+            CheckCorrectSorting(defaultUnsortedArray, new BubbleSort<int>());
+        }
+
+        [Test]
+        public void SortRandomUnsortedArray()
+        {
+            CheckCorrectSorting(GetRandomArray(10, -100, 100), new BubbleSort<int>());
+        }
+
+        [Test]
+        public void NotChangeEmptyArray()
+        {
+            var sortableArray = new SortableArray<int>();
+            Assert.AreEqual(Array.Empty<int>(), sortableArray.GetValues);
+        }
+
+        [Test]
+        public void NotChangeArrayWithOneElement()
+        {
+            var sortableArray = new SortableArray<int>(0);
+            Assert.AreEqual(new [] { 0 }, sortableArray.GetValues);
+        }
+
+        [Test]
+        public void ThrowExceptionIfArrayIsNull()
+        {
+            Assert.Throws<NullReferenceException>(() => new BubbleSort<int>().Sort(null));
+        }
+    }
+}

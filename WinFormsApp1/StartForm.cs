a443d6a73e583d87ecb37eb15a86b1fe735a9d66
QmRite <82218195+QmRite@users.noreply.github.com>
2022-12-28 16:16:24 +0500

    Added parallel sort executor

--- WinFormsApp1/StartForm.cs
+++ WinFormsApp1/StartForm.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,13 +18,14 @@ namespace WinFormsApp1
         public StartForm()
         {
             InitializeComponent();
+            DoubleBuffered = true;
 
             comboBoxes = new System.Windows.Forms.ComboBox[] { comboBox1, comboBox2 }; //массив чекбоксов
             foreach (var cb in comboBoxes)
                 cb.SelectedIndexChanged += someChanged;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private async void button1_Click(object sender, EventArgs e)
         {
             var count = trackBar1.Value;
 
@@ -33,14 +34,15 @@ namespace WinFormsApp1
 
             var visualFirst = new Visualiser<int>(pictureBox1);
             sortFirst.SortableArray.RegisterObserver(visualFirst);
-            sortFirst.Sort();
+            
 
             var type2 = comboBox2.SelectedItem.ToString();
             var sortSecond = SortingFactory.CreateSort(type2, RndArray.Get(count));
 
             var visualSecond = new Visualiser<int>(pictureBox2);
             sortSecond.SortableArray.RegisterObserver(visualSecond);
-            sortSecond.Sort();
+            
+            await ParallelSortExecutor<int>.Execute(sortFirst, sortSecond);
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -66,6 +68,21 @@ namespace WinFormsApp1
             foreach (var cb in comboBoxes)
                 if (cb.SelectedItem is null) return false;
             return true;
+        }
+
+        private void StartForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }

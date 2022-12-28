f197b8c5827e65c86a8144681e3fe1114e8d428b
groMMaster <Grigory.Vinokursky@urfu.me>
2022-12-28 17:52:20 +0500

    Update Vis

--- WinFormsApp1/Visualiser.cs
+++ WinFormsApp1/Visualiser.cs
@@ -30,19 +30,35 @@ namespace WinFormsApp1
             g.Clear(Color.White);
             Pen pen = new Pen(Color.Black, widthPen);
 
-            var x = count / 2;
-            var stepX = (width - widthPen * count) / (count + 1);
+            var xHorizontalMargin = 20;
+            var stepX = (double)(width - 2 * xHorizontalMargin - widthPen * count) / (count - 1);
+            var remStepX = stepX - (int)stepX;
             var y1 = height - 50;
             var yE = (height - 100) / count;
+            var x = xHorizontalMargin;
+            var sumRemStepX = 0.0;
 
             foreach (var e in array)
             {
-                x += stepX;
+                if (sumRemStepX < 2) { sumRemStepX += remStepX; }
+                else
+                {
+                    sumRemStepX-=2;
+                    x+=2;
+                }
+                if (sumRemStepX < 1) { sumRemStepX += remStepX; }
+                else
+                {
+                    sumRemStepX--;
+                    x++;
+                } 
+                    
                 var p1 = new Point(x, y1);
                 var y2 = y1 - yE * (int)(object)e;
                 var p2 = new Point(x, y2);
 
                 g.DrawLine(pen, p1, p2);
+                x += (int)stepX;
                 x += widthPen;
             }
             

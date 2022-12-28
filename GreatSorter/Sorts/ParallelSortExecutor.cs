bf233b33a3a8a270ae6bf448be8002c140a64f27
QmRite <82218195+QmRite@users.noreply.github.com>
2022-12-28 16:14:59 +0500

    Created ParallelSortExecutor

---
+++ GreatSorter/Sorts/ParallelSortExecutor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatSorter
+{
+    public static class ParallelSortExecutor<T> where T : IComparable
+    {
+        public static async Task Execute(SortAlgorithm<T> firstSortAlgorithm, SortAlgorithm<T> secondSortAlgorithm)
+        {
+            var firstSortTask = Task.Run(() => firstSortAlgorithm.Sort());
+            var secondSortTask = Task.Run(() => secondSortAlgorithm.Sort());
+
+            await firstSortTask;
+            await secondSortTask;
+        }
+    }
+}
